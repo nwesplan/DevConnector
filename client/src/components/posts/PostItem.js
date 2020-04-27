48c8f1cac6fafcfd791acd7a34bd08e70eafc158
Nathan Esplan <nwesplan@gmail.com>
2020-04-27 18:29:40 -0500

    prepare deployment

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -58,15 +58,6 @@ const PostItem = ({
                 <i class='fas fa-times'></i>
               </button>
             )}
-            {!auth.loading && user === auth.user._id && (
-              <button
-                onClick={(e) => deletePost(_id)}
-                type='button'
-                class='btn btn-danger'
-              >
-                <i class='fas fa-times'></i>
-              </button>
-            )}
           </Fragment>
         )}
       </div>
